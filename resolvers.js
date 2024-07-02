b23f0db969f6e7521d1594bd00666b48ac48cc18
harith-abeysinghe <e19003@eng.pdn.ac.lk>
2024-07-02 16:17:51 +0530

    Nested Queries

--- resolvers.js
+++ resolvers.js
@@ -12,5 +12,28 @@ export const resolvers = {
 		authors() {
 			return db.authors;
 		},
+		review(_, { id }) {
+			return db.reviews.find((review) => review.id === id);
+		},
+		game(_, { id }) {
+			return db.games.find((game) => game.id === id);
+		},
+		author(_, { id }) {
+			return db.authors.find((author) => author.id === id);
+		},
+	},
+
+	Game: {
+		reviews(game) {
+			return db.reviews.filter((review) => review.game_id === game.id);
+		},
+	},
+	Review: {
+		game(review) {
+			return db.games.find((game) => game.id === review.game_id);
+		},
+		author(review) {
+			return db.authors.find((author) => author.id === review.author_id);
+		},
 	},
 };
--- schema.js
+++ schema.js
@@ -4,18 +4,23 @@ export const typeDefs = `#graphql
         id: ID!,  #having the ! means is that the data field can't be null
         title: String!,
         platform: [String!]!, 
+        reviews: [Review!]
+
     }
 
     type Review{
         id: ID!,
         content: String!
         rating: Int!
+        game: Game!
+        author: Author!
     }
 
     type Author{
         id: ID!,
         name: String!
         verified: Boolean!
+        reviews: [Review!]
     }
 
     #must have a Query type
@@ -24,6 +29,10 @@ export const typeDefs = `#graphql
         games: [Game]
         reviews: [Review]
         authors: [Author]
+
+        review(id: ID!): Review
+        game(id: ID!): Game
+        author(id: ID!): Author
     }
 
 `;
